2f2411072848e64347311ce87bd03141b643942b
Amirhosein1902 <amirhs20055@gmail.com>
2025-02-06 23:20:28 +0330

    Update rogue.h

--- rogue.h
+++ rogue.h
@@ -3,47 +3,77 @@
 
 #include <ncurses.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// color pairs
+#define VISIBLE_COLOR 1
+#define SEEN_COLOR 2
 
 typedef struct
 {
-  int y;
-  int x;
+	int y;
+	int x;
 } Position;
 
-
 typedef struct
 {
-  char ch;
-  bool walkable;
+	char ch;
+	int color;
+	bool walkable;
+	bool transparent;
+	bool visible;
+	bool seen;
 } Tile;
 
+typedef struct
+{
+	int height;
+	int width;
+	Position pos;
+	Position center;
+} Room;
 
 typedef struct
 {
-  Position pos;
-  char ch;
+	Position pos;
+	char ch;
+	int color;
 } Entity;
 
-//draw.c functions+
+//draw
 void drawMap(void);
 void drawEntity(Entity* entity);
 void drawEverything(void);
 
-//engine.c functions
-void cursesSetup(void);
+//engine
+bool cursesSetup(void);
 void gameLoop(void);
 void closeGame(void);
 
-//map.c functions
+//map
 Tile** createMapTiles(void);
 Position setupMap(void);
 void freeMap(void);
 
-// player.c functions
+// player
 Entity* createPlayer(Position start_pos);
 void handleInput(int input);
 void movePlayer(Position newPos);
-// externs
+
+// room
+Room createRoom(int y, int x, int height, int width);
+void addRoomToMap(Room room);
+void connectRoomCenters(Position centerOne, Position centerTwo);
+
+// fov
+void makeFOV(Entity* player);
+void clearFOV(Entity* player);
+int getDistance(Position origin, Position target);
+bool isInMap(int y, int x);
+bool lineOfSight(Position origin, Position target);
+int getSign(int a);
+
 extern const int MAP_HEIGHT;
 extern const int MAP_WIDTH;
 extern Entity* player;
