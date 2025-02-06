d40d4124154adc309a320f4f105aec10b2067a02
Amirhosein1902 <amirhs20055@gmail.com>
2025-02-06 23:18:11 +0330

    Upply

--- player.c
+++ player.c
@@ -3,65 +3,52 @@
 
 Entity* createPlayer(Position start_pos)
 {
-  Entity* newPlayer = calloc(1, sizeof(Entity));
+	Entity* newPlayer = calloc(1, sizeof(Entity));
 
-  newPlayer->pos.y = start_pos.y;
-  newPlayer->pos.x = start_pos.x;
-  newPlayer->ch = '@' ;
+	newPlayer->pos.y = start_pos.y;
+	newPlayer->pos.x = start_pos.x;
+	newPlayer->ch = '@';
+	newPlayer->color = COLOR_PAIR(VISIBLE_COLOR);
 
-  return newPlayer;
+	return newPlayer;
 }
 
 void handleInput(int input)
 {
+	Position newPos = { player->pos.y, player->pos.x };
 
-  Position newPos = { player->pos.y, player->pos.x };	
-  switch(input)
-  {
-    //move up
-    case '8':
-      newPos.y--;
-      break;
-    //move down
-    case '5':
-      newPos.y++;
-      break;
-    //move left
-    case '4':
-      newPos.x--;
-      break;
-    //move right
-    case '6':
-      newPos.x++;
-      break;
-    case '9':
-      newPos.x++;
-      newPos.y--;
-      break;
-    case '7':
-      newPos.x--;
-      newPos.y--;
-      break;
-    case '3':
-      newPos.x++;
-      newPos.y++;
-      break;
-    case '1':
-      newPos.x--;
-      newPos.y++;
-      break;
-    default:
-      break;
-  }
+	switch(input)
+	{
+		//move up
+		case 'w':
+			newPos.y--;
+			break;
+		//move down
+		case 's':
+			newPos.y++;
+			break;
+		//move left
+		case 'a':
+			newPos.x--;
+			break;
+		//move right
+		case 'd':
+			newPos.x++;
+			break;
+		default:
+			break;
+	}
 
-  movePlayer(newPos);
+	movePlayer(newPos);
 }
 
 void movePlayer(Position newPos)
 {
-  if (map[newPos.y][newPos.x].walkable)
-  {
-    player->pos.y = newPos.y;
-    player->pos.x = newPos.x;
-  }
+	if (map[newPos.y][newPos.x].walkable)
+	{
+		clearFOV(player);
+		player->pos.y = newPos.y;
+		player->pos.x = newPos.x;
+		makeFOV(player);
+	}
 }
